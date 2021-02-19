bd408235b19f9f40d3f48b8c78afdc4778253f72
AllyDale <vipally@gmail.com>
2021-02-19 11:26:30 +0800

    1

--- cli/flag.go
+++ cli/flag.go
@@ -1,21 +1,15 @@
 package cli
 
-import (
-	"flag"
-	"fmt"
-)
-
 type Flag struct {
-	LogicName   string
-	Name        string
-	Aliases     []string
-	Usage       string
-	DefaultText string
-	Required    bool
-	Hidden      bool
-	EnvVars     []string
-	FilePath    string
-	Value       Value
-	names       []string
-	logicName   string
+	LogicName string
+	Name      string
+	Aliases   []string
+	Usage     string
+	Required  bool
+	Hidden    bool
+	EnvVars   []string
+	FilePath  string
+	Value     Value
+	names     []string
+	logicName string
 }
--- cli/value.go
+++ cli/value.go
@@ -1,5 +1,10 @@
 package cli
 
+import (
+	"flag"
+	"fmt"
+)
+
 type Value interface {
 	fmt.Stringer
 	Apply(*flag.FlagSet) error // Apply Flag settings to the given flag set
